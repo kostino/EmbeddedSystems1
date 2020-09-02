1c80a9cbad631a4444955fe7f69597b70bd1bf67
kostino <kost.tri@gmail.com>
2020-09-02 17:13:08 +0300

    Fix Typo

--- Timer Exercise/Timer.c
+++ Timer Exercise/Timer.c
@@ -4,7 +4,7 @@
 #include <pthread.h>
 
 int start(timer *t){
-    if(!(pthread_create(/*TID*/, NULL, /*FUNCTION*/, /*args*/)==)){
+    if(!(pthread_create(/*TID*/, NULL, /*FUNCTION*/, /*args*/)==0)){
         return ERROR_PTHREAD_CREATE;
     }
     return 0;
