c26b27ed5c419bf745f2a6752308be96141c8132
kostino <kost.tri@gmail.com>
2020-09-02 15:54:17 +0300

    Add Timer Struct

--- Timer Exercise/prod-cons.c
+++ Timer Exercise/prod-cons.c
@@ -27,6 +27,17 @@ void *producer (void *args);
 void *consumer (void *args);
 
 typedef struct {
+  int period; // Time between calls of TimerFcn in ms
+  int tasks_to_execute; // How many times TimerFcn is ran
+  int start_delay; // Seconds before first start of TimerFcn
+  void (*start_fcn)(); // "Constructor" function for timer objects, init data needed for TimerFnc
+  void (*stop_fcn)(); // Function that runs after last call of TimerFcn
+  void (*timer_fcn)(void*); // Function called at the start of each period
+  void (*error_fcn)(); // Function called if queeu becomes full
+  void *user_data; // Pointer to user data
+} timer;
+
+typedef struct {
   void * (*work)(void *);
   void * arg;
 } workFunction;
